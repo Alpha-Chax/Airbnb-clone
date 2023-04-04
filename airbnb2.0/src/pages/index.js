0f296e3b62becd0d9f196f3d57172ecd9b67ae57
purav.kapoor <puravkapoor77@gmail.com>
2023-04-04 19:00:28 +0530

    Created Banner

--- airbnb2.0/src/pages/index.js
+++ airbnb2.0/src/pages/index.js
@@ -1,5 +1,6 @@
 import Head from "next/head";
 import Header from "../../components/Header";
+import Banner from "../../components/Banner";
 
 export default function Home() {
   return (
@@ -9,6 +10,8 @@ export default function Home() {
       </Head>
 
       <Header />
+      {/* Banner */}
+      <Banner />
     </div>
   );
 }
