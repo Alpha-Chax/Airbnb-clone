3fe54e9ddff5fb815ebebae9f016ccfd5d378064
purav.kapoor <puravkapoor77@gmail.com>
2023-04-07 11:38:44 +0530

    Small cards (main section)

--- airbnb2.0/components/SmallCard.js
+++ airbnb2.0/components/SmallCard.js
@@ -3,7 +3,7 @@ import Image from "next/image";
 
 function SmallCard({ image, distance, location }) {
   return (
-    <div className="flex">
+    <div className="flex items-center m-2 space-x-4 mt-5 rounded-xl cursor-pointer hover:bg-gray-100 hover:scale-105 transition transform duration-200 ease-out">
       {/* Left */}
       <div className="relative h16 w-16 ">
         <Image
@@ -19,7 +19,7 @@ function SmallCard({ image, distance, location }) {
       {/* Right */}
       <div>
         <h2>{location}</h2>
-        <h3>{distance}</h3>
+        <h3 className="text-gray-500">{distance}</h3>
       </div>
     </div>
   );
--- airbnb2.0/src/pages/index.js
+++ airbnb2.0/src/pages/index.js
@@ -16,14 +16,20 @@ export default function Home({ exploreData }) {
       <main className="max-w-7xl mx-auto px-8 sm:px-16">
         <section className="pt-6">
           <h2 className="text-4xl font-semibold pb-5">Explore Nearby</h2>
-          {exploreData?.map(({ img, distance, location }, i) => (
-            <SmallCard
-              key={i}
-              image={img}
-              distance={distance}
-              location={location}
-            />
-          ))}
+          {/* Small Card Mapped */}
+          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+            {exploreData?.map(({ img, distance, location }, i) => (
+              <SmallCard
+                key={i}
+                image={img}
+                distance={distance}
+                location={location}
+              />
+            ))}
+          </div>
+        </section>
+        <section>
+          <h2 className="text-4xl font-semibold py-8">Live Anywhere</h2>
         </section>
       </main>
     </div>
